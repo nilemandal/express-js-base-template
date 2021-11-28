e22d6c5708665b319d8cd5a83575c648218341a0
Nilesh Mandal <nilesh@devcentrehouse.eu>
2021-11-28 11:59:05 +0000

    basic routing added

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(urlencodedParser);
+
 app.get('/' , (req,res)=>{
     console.log('Hello Nile');
     res.send('Hello world');
+})
+
+app.get('/get-request', (req,res)=>{
+    console.log('GET Request');
+    res.send(`GET Request for ${req.query.name} at ${new Date()}`);
+})
+
+app.post('/post-request', (req,res)=>{
+    console.log('POST Request' , req.body);
+    res.send(`POST Response for ${req.body?.name} at ${new Date()}`)
 })
 
 app.listen(port , ()=>{
