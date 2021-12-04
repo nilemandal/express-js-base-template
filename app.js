64a0deb63944feb7b61af40d6137555b2c117340
Nilesh Mandal <nilesh@devcentrehouse.eu>
2021-12-04 05:23:05 +0000

    mysql db connection

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ const logger = (req,res,next) =>{
     console.log('Request hit at' , new Date());
     next()
 }
+
+app.set('view engine', 'pug')
 app.get('/' , logger, (req,res)=>{
-    res.send('Hello world');
+    res.render('index', { title: 'Express Js', message: 'Hello there! Welcome to Relinns Technology' })
 })
 
 app.get('/get-request', (req,res)=>{
@@ -28,6 +30,31 @@ app.post('/post-request', (req,res)=>{
     res.send(`POST Response for ${req.body?.name} at ${new Date()}`)
 })
 
-app.listen(port , ()=>{
-    console.log(`App is running at http://localhost:${port}`)
-})
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: "test_db",
+    port:8889
+  });
+
+connection.connect(error => {
+    if (error){
+        console.log("An error occurred");
+        throw error;
+    }
+
+    app.listen(port, ()=>{
+        console.log("Application listening at http://localhost:"+port);
+    })
+});
+
+app.get('/get-db-data' , async (req,res)=>{
+    connection.query('SELECT * FROM test_table', (err, rows, fields)=>{
+        if (err) throw err
+
+        if(rows[0]) return res.send({status:'success', data: rows})
+        else return res.send({status:'error'});
+    })
+});
